07636b37264a7b8c37980589bee4e531973e1a6a
fahim-90 <fahimcse.nubngpi@gmail.com>
2024-09-26 19:51:46 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+$(".slide-active").slick({
+  infinite: true,
+  arrows: false,
+  dots: true,
+  slidesToShow: 1,
+  fade: true,
+  autoplay: true,
+  slidesToScroll: 1,
+  speed: 3000,
+});
+
+$(".category_active").slick({
+  infinite: true,
+  arrows: false,
+  dots: false,
+  slidesToShow: 6,
+  autoplay: true,
+  slidesToScroll: 1,
+  speed: 3000,
+});
